952a03b5519659f6639de05a804c52b5c2318835
rybalkomihail744@gmail.com <rybalkomihail744@gmail.com>
2023-12-15 12:11:40 +0300

    Юнит тесты

---
+++ TestClass/TestClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Runtime.Remoting.Messaging;
+
+namespace TestClass
+{
+    public class TestClass
+    {
+        public static bool authorization(string login, string password)
+        {
+            if (login == "" || password == "")
+            {
+                return false;
+            }
+            return true;
+        }
+        public static int intNumber(string textNumber)
+        {
+            Regex intNumber = new Regex(@"^\d+$");
+            MatchCollection matches;
+            matches = intNumber.Matches(textNumber);
+            if (matches.Count > 0)
+            {
+                return int.Parse(textNumber);
+            }
+            return -1;
+        }
+        public static double doubleNumber(string textNumber)
+        {
+            Regex doubleNumber = new Regex(@"^\d+(,\d+)?$");
+            MatchCollection matches;
+            matches = doubleNumber.Matches(textNumber);
+            if (matches.Count > 0)
+            {
+                return double.Parse(textNumber);
+            }
+            return -1.0;
+        }
+
+    }
+}
--- ToolShopTest/UnitTest1.cs
+++ ToolShopTest/UnitTest1.cs
@@ -2,24 +2,78 @@
 using System;
 using ToolShop;
 using ToolShop.Pages;
+using TestClass;
 
 namespace ToolShopTest
 {
-    [TestClass]
+    [TestClass()]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestGooLogin()
+        bool answer;
+        string login;
+        string password;
+        string intNumber;
+        string doubleNumber;
+
+        [TestMethod()]
+        public void AuthorizationTest1()
         {
-            AuthorizationPage authorizationPage = new AuthorizationPage();
-            authorizationPage.loginBox.Text = "1";
-            authorizationPage.passwordBox.Password = "1";
-
-            authorizationPage.loginButton_Click(null, null);
-
-            Assert.IsNotNull(App.CurrentUser);
-
-            Assert.AreEqual(true, oUser.Login("admin", "admin"), "admin");
+            login = "";
+            password = "";
+            answer = TestClass.TestClass.authorization(login, password);
+            Assert.IsFalse(answer);
+        }
+        [TestMethod()]
+        public void AuthorizationTest2()
+        {
+            login = "1";
+            password = "";
+            answer = TestClass.TestClass.authorization(login, password);
+            Assert.IsFalse(answer);
+        }
+        [TestMethod()]
+        public void AuthorizationTest3()
+        {
+            login = "1";
+            password = "1";
+            answer = TestClass.TestClass.authorization(login, password);
+            Assert.IsTrue(answer);
+        }
+        [TestMethod()]
+        public void CorrectIntNumber1()
+        {
+            intNumber = "12";
+            Assert.AreEqual(12, TestClass.TestClass.intNumber(intNumber));
+        }
+        [TestMethod()]
+        public void CorrectIntNumber2()
+        {
+            intNumber = "12,1";
+            Assert.AreEqual(-1, TestClass.TestClass.intNumber(intNumber));
+        }
+        [TestMethod()]
+        public void CorrectIntNumber3()
+        {
+            intNumber = "0";
+            Assert.AreEqual(0, TestClass.TestClass.intNumber(intNumber));
+        }
+        [TestMethod()]
+        public void CorrectDoubleNumber1()
+        {
+            doubleNumber = "12.3";
+            Assert.AreEqual(-1.0, TestClass.TestClass.doubleNumber(doubleNumber));
+        }
+        [TestMethod()]
+        public void CorrectDoubleNumber2()
+        {
+            doubleNumber = "12,3";
+            Assert.AreEqual(12.3, TestClass.TestClass.doubleNumber(doubleNumber));
+        }
+        [TestMethod()]
+        public void CorrectDoubleNumber3()
+        {
+            doubleNumber = "asdas";
+            Assert.AreEqual(-1.0, TestClass.TestClass.doubleNumber(doubleNumber));
         }
     }
 }
